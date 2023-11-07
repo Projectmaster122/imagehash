df0d579c1fd05a52bfbec6a462d23b2264b2f7f0
Fakie <68382320+Projectmaster122@users.noreply.github.com>
2023-11-07 17:57:12 -0500

    added magic number header, used canon40d image for tests

--- converter.py
+++ converter.py
@@ -1,31 +1,36 @@
-from PIL import Image
+from PIL import Image, ExifTags
 import hashlib
 
-image = Image.open("colorful_board.png")
-pixel_data = list(image.getdata())
+#magic number define
+magicnumber = "PHOTODNA0.0.1"
+magicnum_hex = magicnumber.encode().hex()
 
+#open image and get exif
+img = Image.open("Canon_40D.jpg")
+exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
+
+#get pixel data and turn it into hex, then take it out of a list
+pixel_data = list(img.getdata())
 hex_pixel_data = [f"{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}" for rgb in pixel_data]
+hex_string = ''.join(hex_num for hex_num in hex_pixel_data)
 
-hex_string = hex(int(''.join(hex_num for hex_num in hex_pixel_data)))
-
-
-address = "123,ExampleStreet,US"
-date = "6_11_2023"
-camera = "CAN_EOS_R"
+# address = "123,ExampleStreet,US"
+date = exif['DateTime']
+camera = exif['Model']
 
 address_hex = address.encode().hex()
 date_hex = date.encode().hex()
 camera_hex = camera.encode().hex()
 
-metadata_string = address_hex + "00" + date_hex + "00" + camera_hex + "01"
+metadata_string = date_hex + "00" + camera_hex + "01"
 
 metadata_bytes = bytes.fromhex(metadata_string)
 metadata_hex = metadata_bytes.hex()
 
-final_hex = metadata_hex + hex_string
+final_hex = magicnum_hex + "03" + metadata_hex + hex_string
 
 
 hash_object = hashlib.sha256(final_hex.encode())
 hash_value = hash_object.hexdigest()
 
-print(hash_value)
+print(final_hex)

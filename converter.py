6d459f5267f31733ac510fde1a4fc6599a311643
Fakie <eitan.collett@gmail.com>
2023-11-10 16:15:35 -0500

    e265, e308, e305, e302, added sample images

--- converter.py
+++ converter.py
@@ -1,19 +1,19 @@
 from PIL import Image, ExifTags
 import hashlib
 
-def hashmaker():
 
-    #just in case
+def hashmaker():
+    # just in case
     exif2 = {
         "Model": "Unknown",
         "DateTime": "Unknown"
     }
 
-    #magic number define
+    # magic number define
     magicnumber = "IMAGEHASH0.0.2"
     magicnum_hex = magicnumber.encode().hex()
 
-    #open image and get exif
+    # open image and get exif
     imgname = "Canon_40D.jpg"
     img = Image.open(imgname)
     exif = ""
@@ -22,30 +22,31 @@ def hashmaker():
     except:
         exif = exif2
 
-    #get pixel data and turn it into hex, then take it out of a list
+    # get pixel data and turn it into hex, then take it out of a list
     pixel_data = list(img.getdata())
     hex_pixel_data = [f"{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}" for rgb in pixel_data]
     hex_string = ''.join(hex_num for hex_num in hex_pixel_data)
 
-    # TODO: address = exif['Address']
+    # exif stuff
     date = exif['DateTime']
     camera = exif['Model']
 
     date_hex = date.encode().hex()
     camera_hex = camera.encode().hex()
-
+    # generate metadata string
     metadata_string = date_hex + "00" + camera_hex + "01"
 
     metadata_bytes = bytes.fromhex(metadata_string)
     metadata_hex = metadata_bytes.hex()
-
+    # final hex string generation
     final_hex = magicnum_hex + "03" + metadata_hex + hex_string
 
-
+    # hash value generation
     hash_object = hashlib.sha256(final_hex.encode())
     hash_value = hash_object.hexdigest()
 
     main(date, camera, date_hex, camera_hex, metadata_hex, final_hex, hash_value, magicnumber, magicnum_hex, imgname)
+
 
 def main(date, camera, date_hex, camera_hex, metadata_hex, final_hex, hash_value, magicnumber, magicnum_hex, imgname):
     while True:
@@ -66,6 +67,7 @@ def main(date, camera, date_hex, camera_hex, metadata_hex, final_hex, hash_value
             print(f"{magicnum_hex}/{magicnumber}")
         elif cmd == "img":
             print(f"{imgname}")
+
 
 if __name__ == "__main__":
     hashmaker()
